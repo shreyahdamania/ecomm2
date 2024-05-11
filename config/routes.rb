7e533588a816d7159903b28b4b1c3cf5a061bc95
Shreyash <144013509+shreyahdamania@users.noreply.github.com>
2024-05-11 15:23:44 +0530

    Made admin navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :categories
-  end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
