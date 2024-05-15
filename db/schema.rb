07507ec13cdee6e359c76270bd086095382f902c
Shreyash <144013509+shreyahdamania@users.noreply.github.com>
2024-05-15 20:04:44 +0530

    Created and setup order section

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_15_090051) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_101633) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -61,6 +61,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_15_090051) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "customer_email"
+    t.boolean "fulfilled"
+    t.integer "total"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
