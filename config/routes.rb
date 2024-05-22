48a0a73482f318fc236d700da4b808832f44342d
Shreyash <144013509+shreyahdamania@users.noreply.github.com>
2024-05-22 12:07:55 +0530

    Added webhooks route, which updates the stocks and creates orders after payment is successful, used stripe cli to test it

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :orders
-    
     resources :products do
       resources :stocks
     end
-    resources :categories
+      resources :categories
   end
   devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
+  get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   root "home#index"
@@ -20,12 +19,14 @@ Rails.application.routes.draw do
   authenticated :admin_user do
     root to: "admin#index", as: :admin_root
   end
-  
+
   resources :categories, only: [:show]
   resources :products, only: [:show]
 
   get "admin" => "admin#index"
   get "cart" => "carts#show"
   post "checkout" => "checkouts#create"
-  
+  get "success" => "checkouts#success"
+  get "cancel" => "checkouts#cancel"
+  post "webhooks" => "webhooks#stripe"
 end
